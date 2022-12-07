01e5dae90c4afa5029335652b8b6b4904317bc2a
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-12-07 18:32:04 +0100

    Incorporamos la clase principal donde esta el metodo create()

---
+++ core/src/main/java/es/iesoretania/dam2/hlc/Proyecto2DGame.java
@@ -0,0 +1,31 @@
+package es.iesoretania.dam2.hlc;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class Proyecto2DGame extends Game {
+	BitmapFont font;
+	SpriteBatch batch;
+	ShapeRenderer shapeRenderer;
+	public Skin gameSkin;
+
+	@Override
+	public void create() {
+		font = new BitmapFont();
+		batch = new SpriteBatch();
+		shapeRenderer = new ShapeRenderer();
+		gameSkin = new Skin(Gdx.files.internal(""));
+		setScreen(new MenuScreen(this));
+	}
+
+	@Override
+	public void dispose() {
+		font.dispose();
+		batch.dispose();
+	}
+}
--- lwjgl3/src/main/java/es/iesoretania/dam2/hlc/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/es/iesoretania/dam2/hlc/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package es.iesoretania.dam2.hlc.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import es.iesoretania.dam2.hlc.Proyecto2D;
+import es.iesoretania.dam2.hlc.Proyecto2DGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -11,7 +11,7 @@ public class Lwjgl3Launcher {
 	}
 
 	private static Lwjgl3Application createApplication() {
-		return new Lwjgl3Application(new Proyecto2D(), getDefaultConfiguration());
+		return new Lwjgl3Application(new Proyecto2DGame(), getDefaultConfiguration());
 	}
 
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
